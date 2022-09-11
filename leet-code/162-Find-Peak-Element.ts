6231b8aab69d876bb501b4976b7fc3bcbb319e5a
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-09-11 17:53:22 -0300

    improve 162

--- leet-code/162-Find-Peak-Element.ts
+++ leet-code/162-Find-Peak-Element.ts
@@ -1,22 +1,38 @@
+// function findPeakElement(nums: number[]): number {
+//   let low = 0;
+//   let high = nums.length - 1;
+
+//   while (low < high) {
+//     const mid = Math.floor((low + high) / 2);
+//     const leftValue = nums.at(mid - 1);
+//     const rightValue = nums.at(mid + 1);
+
+//     if (nums[mid] > rightValue && nums[mid] > leftValue) {
+//       return mid;
+//     }
+
+//     if (nums[mid] < rightValue) {
+//       low = mid + 1;
+//     } else if (nums[mid] < leftValue) {
+//       high = mid - 1;
+//     }
+//   }
+
+//   return low;
+// }
+
 function findPeakElement(nums: number[]): number {
-  let low = 0;
-  let high = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
 
-  while (low < high) {
-    const mid = Math.floor((low + high) / 2);
-    const leftValue = nums.at(mid - 1);
-    const rightValue = nums.at(mid + 1);
-
-    if (nums[mid] > rightValue && nums[mid] > leftValue) {
-      return mid;
-    }
-
-    if (nums[mid] < rightValue) {
-      low = mid + 1;
-    } else if (nums[mid] < leftValue) {
-      high = mid - 1;
+  while (left < right) {
+    const middle = Math.floor((left + right) / 2);
+    if (nums[middle] < nums[middle + 1]) {
+      left = middle + 1;
+    } else {
+      right = middle;
     }
   }
 
-  return low;
+  return left;
 }
