0ab06e17b085d266d5fa27b007541b895f9d78a4
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-09-25 12:51:20 -0300

    improve 49 algorithm with O(1) memory solution

--- leet-code/49-Group-Anagrams.ts
+++ leet-code/49-Group-Anagrams.ts
@@ -1,21 +1,65 @@
-function groupAnagrams(strs: string[]): string[][] {
-  const map = {};
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+// function setZeroes(matrix: number[][]): void {
+//     const rowsCount = matrix.length
+//     const columnsCount = matrix[0].length
+//     const positionsWithZero: number[][] = []
 
-  for (let str of strs) {
-    const count: number[] = Array(26).fill(0);
+//     for(let i = 0; i < rowsCount; i++) {
+//         for(let j = 0; j < columnsCount; j++) {
+//             if(matrix[i][j] === 0) {
+//                 positionsWithZero.push([i,j])
+//             }
+//         }
+//     }
 
-    for (let char of str.split('')) {
-      const pos = char.charCodeAt(0) - 'a'.charCodeAt(0);
-      count[pos]++;
-    }
+//     for(let position of positionsWithZero) {
+//         for(let i = 0; i < rowsCount; i++) {
+//             matrix[i][position[1]] = 0
+//         }
 
-    const key = count.toString();
-    if (map[key]) {
-      map[key].push(str);
-    } else {
-      map[key] = [str];
+//         for(let j = 0; j < columnsCount; j++) {
+//             matrix[position[0]][j] = 0
+//         }
+//     }
+// };
+
+function setZeroes(matrix: number[][]): void {
+  const rowsCount = matrix.length;
+  const columnsCount = matrix[0].length;
+  let firstRow = false;
+
+  for (let i = 0; i < rowsCount; i++) {
+    for (let j = 0; j < columnsCount; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[0][j] = 0;
+        if (i === 0) {
+          firstRow = true;
+        } else {
+          matrix[i][0] = 0;
+        }
+      }
     }
   }
 
-  return Object.values(map);
+  for (let i = 1; i < rowsCount; i++) {
+    for (let j = 1; j < columnsCount; j++) {
+      if (matrix[0][j] === 0 || matrix[i][0] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (matrix[0][0] === 0) {
+    for (let i = 1; i < rowsCount; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  if (firstRow) {
+    for (let i = 0; i < columnsCount; i++) {
+      matrix[0][i] = 0;
+    }
+  }
 }
